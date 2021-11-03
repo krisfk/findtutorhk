757c7560386f8dd4f2a489db1611bedc2e445f79
kaiyu li <krisfk@gmail.com>
2021-11-03 18:08:12 +0800

    .

--- wp-content/themes/findtutorhk/index.php
+++ wp-content/themes/findtutorhk/index.php
@@ -39,11 +39,37 @@ get_header(); ?>
     </div>
     <h2 class="text-center">最新補習個案</h2>
 
-    <a href="#">
-        <img class="dummy-case-img"
-            src="https://findtutorhk.com/wp-content/uploads/2021/11/Screenshot-2021-11-03-at-6.05.26-PM.png" alt="">
+    <div class="row">
 
-    </a>
+        <div class="col-3"> <a href="#">
+                <img class="dummy-case-img"
+                    src="https://findtutorhk.com/wp-content/uploads/2021/11/Screenshot-2021-11-03-at-6.05.26-PM.png"
+                    alt="">
+            </a>
+
+        </div>
+        <div class="col-3"> <a href="#">
+                <img class="dummy-case-img"
+                    src="https://findtutorhk.com/wp-content/uploads/2021/11/Screenshot-2021-11-03-at-6.05.26-PM.png"
+                    alt="">
+            </a>
+
+        </div>
+        <div class="col-3"> <a href="#">
+                <img class="dummy-case-img"
+                    src="https://findtutorhk.com/wp-content/uploads/2021/11/Screenshot-2021-11-03-at-6.05.26-PM.png"
+                    alt="">
+            </a>
+
+        </div>
+        <div class="col-3"> <a href="#">
+                <img class="dummy-case-img"
+                    src="https://findtutorhk.com/wp-content/uploads/2021/11/Screenshot-2021-11-03-at-6.05.26-PM.png"
+                    alt="">
+            </a>
+
+        </div>
+    </div>
 
 
 
