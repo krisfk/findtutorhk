ed3ab8c28d42aeef853d0c6f2ddc69a668074475
kaiyu li <krisfk@gmail.com>
2021-11-03 20:56:01 +0800

    .

---
+++ wp-content/themes/findtutorhk/page-home.php
@@ -0,0 +1,174 @@
+<?php
+/**
+ * The main template file
+ *
+ * This is the most generic template file in a WordPress theme
+ * and one of the two required files for a theme (the other being style.css).
+ * It is used to display a page when nothing more specific matches a query.
+ * E.g., it puts together the home page when no home.php file exists.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ * @package WordPress
+ * @subpackage Twenty_Twenty_One
+ * @since Twenty Twenty-One 1.0
+ */
+
+get_header(); ?>
+
+<div class="container mt-4">
+
+    <div class="row">
+
+        <div class="col-12 text-center mb-5">
+
+            <div class="blk">
+                whatsapp 查詢: 9999 9999
+            </div>
+
+            <a href="" class="blk">
+                家長找導師
+            </a>
+            <a href="" class="blk">
+
+                新導師登記
+
+            </a>
+        </div>
+
+    </div>
+    <h2 class="text-center mb-3">最新補習個案</h2>
+
+    <div class="row">
+
+        <div class="col-3 home-tutor-case-div"> <a href="#">
+                <img class="dummy-case-img"
+                    src="https://findtutorhk.com/wp-content/uploads/2021/11/Screenshot-2021-11-03-at-6.05.26-PM.png"
+                    alt="">
+            </a>
+
+        </div>
+        <div class="col-3 home-tutor-case-div"> <a href="#">
+                <img class="dummy-case-img"
+                    src="https://findtutorhk.com/wp-content/uploads/2021/11/Screenshot-2021-11-03-at-6.05.26-PM.png"
+                    alt="">
+            </a>
+
+        </div>
+        <div class="col-3 home-tutor-case-div"> <a href="#">
+                <img class="dummy-case-img"
+                    src="https://findtutorhk.com/wp-content/uploads/2021/11/Screenshot-2021-11-03-at-6.05.26-PM.png"
+                    alt="">
+            </a>
+
+        </div>
+        <div class="col-3 home-tutor-case-div"> <a href="#">
+                <img class="dummy-case-img"
+                    src="https://findtutorhk.com/wp-content/uploads/2021/11/Screenshot-2021-11-03-at-6.05.26-PM.png"
+                    alt="">
+            </a>
+
+        </div>
+        <div class="col-3 home-tutor-case-div"> <a href="#">
+                <img class="dummy-case-img"
+                    src="https://findtutorhk.com/wp-content/uploads/2021/11/Screenshot-2021-11-03-at-6.05.26-PM.png"
+                    alt="">
+            </a>
+
+        </div>
+        <div class="col-3 home-tutor-case-div"> <a href="#">
+                <img class="dummy-case-img"
+                    src="https://findtutorhk.com/wp-content/uploads/2021/11/Screenshot-2021-11-03-at-6.05.26-PM.png"
+                    alt="">
+            </a>
+
+        </div>
+        <div class="col-3 home-tutor-case-div"> <a href="#">
+                <img class="dummy-case-img"
+                    src="https://findtutorhk.com/wp-content/uploads/2021/11/Screenshot-2021-11-03-at-6.05.26-PM.png"
+                    alt="">
+            </a>
+
+        </div>
+        <div class="col-3 home-tutor-case-div"> <a href="#">
+                <img class="dummy-case-img"
+                    src="https://findtutorhk.com/wp-content/uploads/2021/11/Screenshot-2021-11-03-at-6.05.26-PM.png"
+                    alt="">
+            </a>
+
+        </div>
+
+    </div>
+
+
+
+    <h2 class="text-center mb-3">最新導師</h2>
+
+    <div class="row">
+        <div class="col-3 home-tutor-case-div"> <a href="#">
+                <img class="dummy-case-img"
+                    src="https://findtutorhk.com/wp-content/uploads/2021/11/Screenshot-2021-11-03-at-6.10.35-PM.png"
+                    alt="">
+            </a>
+
+        </div>
+        <div class="col-3 home-tutor-case-div"> <a href="#">
+                <img class="dummy-case-img"
+                    src="https://findtutorhk.com/wp-content/uploads/2021/11/Screenshot-2021-11-03-at-6.10.35-PM.png"
+                    alt="">
+            </a>
+
+        </div>
+        <div class="col-3 home-tutor-case-div"> <a href="#">
+                <img class="dummy-case-img"
+                    src="https://findtutorhk.com/wp-content/uploads/2021/11/Screenshot-2021-11-03-at-6.10.35-PM.png"
+                    alt="">
+            </a>
+
+        </div>
+        <div class="col-3 home-tutor-case-div"> <a href="#">
+                <img class="dummy-case-img"
+                    src="https://findtutorhk.com/wp-content/uploads/2021/11/Screenshot-2021-11-03-at-6.10.35-PM.png"
+                    alt="">
+            </a>
+
+        </div>
+        <div class="col-3 home-tutor-case-div"> <a href="#">
+                <img class="dummy-case-img"
+                    src="https://findtutorhk.com/wp-content/uploads/2021/11/Screenshot-2021-11-03-at-6.10.35-PM.png"
+                    alt="">
+            </a>
+
+        </div>
+        <div class="col-3 home-tutor-case-div"> <a href="#">
+                <img class="dummy-case-img"
+                    src="https://findtutorhk.com/wp-content/uploads/2021/11/Screenshot-2021-11-03-at-6.10.35-PM.png"
+                    alt="">
+            </a>
+
+        </div>
+        <div class="col-3 home-tutor-case-div"> <a href="#">
+                <img class="dummy-case-img"
+                    src="https://findtutorhk.com/wp-content/uploads/2021/11/Screenshot-2021-11-03-at-6.10.35-PM.png"
+                    alt="">
+            </a>
+
+        </div>
+        <div class="col-3 home-tutor-case-div"> <a href="#">
+                <img class="dummy-case-img"
+                    src="https://findtutorhk.com/wp-content/uploads/2021/11/Screenshot-2021-11-03-at-6.10.35-PM.png"
+                    alt="">
+            </a>
+
+        </div>
+
+    </div>
+
+
+
+</div>
+
+
+<?php
+get_footer();
+?>
