40016e6134b891568b74e0ff63a9b3e23680a4bb
kaiyu li <krisfk@gmail.com>
2021-11-11 23:24:17 +0800

    .

--- wp-content/themes/findtutorhk/page-test.php
+++ wp-content/themes/findtutorhk/page-test.php
@@ -9,6 +9,23 @@ echo do_shortcode('[contact-form-7 id="64" title="student register"]');
 ?>
 
 
+
+<script type="text/javascript">
+var $ = jQuery;
+
+$(function() {
+    var foundin = $('#district-select option:contains("optgroup-")');
+    $.each(foundin, function(value) {
+        var updated = $(this).val().replace('optgroup-', '');
+        $(this).nextUntil('option:contains("endoptgroup")').wrapAll('<optgroup label="' + updated +
+            '"></optgroup>');
+    });
+    $('#district-select option:contains("optgroup-")').remove();
+    $('#district-select option:contains("endoptgroup")').remove();
+});
+</script>
 </body>
+
+
 
 </html>
