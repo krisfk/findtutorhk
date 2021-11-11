84a3c3ac965a3c2e0b3a81d29a09e89f67bee8a3
kaiyu li <krisfk@gmail.com>
2021-11-11 23:37:49 +0800

    .

--- wp-content/themes/findtutorhk/page-test.php
+++ wp-content/themes/findtutorhk/page-test.php
@@ -14,14 +14,14 @@ echo do_shortcode('[contact-form-7 id="64" title="student register"]');
 var $ = jQuery;
 
 $(function() {
-    var foundin = $('#location option:contains("optgroup-")');
+    var foundin = $('select[name="location"] option:contains("optgroup-")');
     $.each(foundin, function(value) {
         var updated = $(this).val().replace('optgroup-', '');
         $(this).nextUntil('option:contains("endoptgroup")').wrapAll('<optgroup label="' + updated +
             '"></optgroup>');
     });
-    $('#location option:contains("optgroup-")').remove();
-    $('#location option:contains("endoptgroup")').remove();
+    $('select[name="location"] option:contains("optgroup-")').remove();
+    $('select[name="location"] option:contains("endoptgroup")').remove();
 });
 </script>
 </body>
