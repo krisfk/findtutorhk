e80262378b48de634ef1aa5dbdc223641e6e79c7
kaiyu li <krisfk@gmail.com>
2021-11-11 23:10:32 +0800

    .

--- wp-content/themes/findtutorhk/page-test.php
+++ wp-content/themes/findtutorhk/page-test.php
@@ -1,16 +1,10 @@
-<!DOCTYPE html>
-<html lang="en">
+<?php
 
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
+get_header();
 
-<body>
+?>
 
-    <?php
+<?php
 echo do_shortcode('[contact-form-7 id="64" title="student register"]');
 ?>
 
