26176cefe5553ad05d454a07e6fb9aeea7317f70
kaiyu li <krisfk@gmail.com>
2021-11-13 05:57:49 +0800

    .

--- wp-content/themes/findtutorhk/functions.php
+++ wp-content/themes/findtutorhk/functions.php
@@ -714,11 +714,11 @@ add_action('cf7_2_post_form_submitted_to_tutor', 'new_tutor_mapped',10,4);
 function new_tutor_mapped($post_id, $cf7_form_data, $cf7form_key, $submitted_files){
 
 
-	print_r($submitted_files);
+	// print_r($submitted_files);
 	// echo $submitted_files['cvfile'][0];
 // echo $post_id;
 //field_618eda4d14ef9 cv
-	update_field( 'field_618eda4d14ef9', 'https://findtutorhk.com/wp-content/uploads/2021/11/WhatsApp-Image-2021-11-05-at-6.22.44-PM.jpeg', $post_id );
+	update_field( 'field_618eda4d14ef9', 83, $post_id );
 
 
 	// echo  '<script type="text/javascript">';
